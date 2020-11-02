27a3be429ad4f3c2e7f3ac4ee5fb40f080c809ab
Tom Svecak <tsvecak@gmail.com>
2020-11-02 14:57:04 +0000

    Well token

--- frontends/bulk-assign/src/WebSummit-bulk-assign.tsx
+++ frontends/bulk-assign/src/WebSummit-bulk-assign.tsx
@@ -8,7 +8,7 @@ const lifecycles = singleSpaReact({
   React,
   ReactDOM,
   rootComponent: (props) => (
-    <App {...props} token="eyJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJ3ZWJzdW1taXQiLCJqdGkiOiIwMGJlZTY3OS1jZDE0LTQwZWUtYTc3Yi0wZTI1NmQyNjgxNTQiLCJpYXQiOjE2MDM4MDk2NTYsImV4cCI6MTYwNDQxNDQ1NiwiZW1haWwiOiJ0b21pc2xhdi5zdmVjYWtAd2Vic3VtbWl0LmNvbSIsImNvbmZfc2x1ZyI6InJjMjEiLCJzZXJ2ZXIiOiJ0aWNrZXQtYXNzaWdubWVudCJ9.hy7W1DN7C7A2ItcqtrpuWHPXm9xZUipvdDvdGY0s2_I"/>
+    <App {...props} token="12345"/>
   ),
   errorBoundary(err, info, props) {
     return null;
