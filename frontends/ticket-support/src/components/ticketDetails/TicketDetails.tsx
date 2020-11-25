f5263499592437fa529eb8d46dbb7a5377611b01
joanmc <joan.mccarthy@websummit.com>
2020-11-25 14:09:50 +0000

    assign & reassign

--- frontends/ticket-support/src/components/ticketDetails/TicketDetails.tsx
+++ frontends/ticket-support/src/components/ticketDetails/TicketDetails.tsx
@@ -1,13 +1,30 @@
 import { ApolloError, useQuery } from '@apollo/client'
-import React, { useContext } from 'react'
+import React, { useContext, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import styled from 'styled-components'
 import TICKET from '../../operations/queries/Ticket'
 import { AppContext, Ticket } from '../app/App'
+import AssignTicket from '../ticketActions/AssignTicket'
+
+const Button = styled.button`
+  margin: 1rem 0;
+  padding: 0.5rem 1rem;
+  border-radius: 8px;
+  border: none;
+  border: 1px solid grey;
+  background: white;
+  cursor: pointer;
+  transition: all 0.3s;
+  &:hover {
+    background-color: grey;
+    color: white;
+  }
+`
 
 const ticketDetails: React.FC = () => {
   const { bookingRef } = useParams<{ bookingRef: string }>()
   const { conferenceSlug, token } = useContext(AppContext)
+  const [reassignment, setReassignment] = useState(false)
 
   const {
     loading,
@@ -40,11 +57,22 @@ const ticketDetails: React.FC = () => {
         <>
           <span>Ticket status: {ticket?.state}</span>
 
+          {ticket && !assignment && (
+            <AssignTicket ticketId={ticket.id} resetReassignment={setReassignment} />
+          )}
+
           {assignee && (
             <>
               <h3>Ticket access information</h3>
               <span>Booking reference: {assignee?.email}</span>
               <span>App login email: {assignment?.appLoginEmail || assignee?.email}</span>
+
+              <Button onClick={() => setReassignment(!reassignment)}>
+                {reassignment ? 'Cancel' : 'Reassign'}
+              </Button>
+              {ticket && reassignment && (
+                <AssignTicket ticketId={ticket.id} resetReassignment={setReassignment} />
+              )}
 
               <h3>User account information</h3>
               <span>Identity email: {assignee?.email}</span>
