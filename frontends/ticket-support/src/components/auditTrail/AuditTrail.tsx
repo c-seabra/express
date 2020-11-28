8c664926d8a3ca2f1d651a1a594ec19b0b2f1454
Tom Svecak <tsvecak@gmail.com>
2020-11-28 19:21:33 +0000

    Well removing stuff

--- frontends/ticket-support/src/components/auditTrail/AuditTrail.tsx
+++ frontends/ticket-support/src/components/auditTrail/AuditTrail.tsx
@@ -41,19 +41,18 @@ const Trail = styled.div`
     background-color: #fff;
   }
 `
-const Column = styled.div`
+export const Column = styled.div`
   width: 10%;
   padding: 0 0.25rem;
   word-break: break-word;
+  display: flex;
+  align-items: center;
 `
-const MediumColumn = styled(Column)`
+export const MediumColumn = styled(Column)`
   width: 25%;
 `
-const WideColumn = styled(Column)`
-  width: 35%;
-`
 
-const AuditTrail = ({bookingRef = "CSRL-1", conferenceSlug = "ws20", token = "eyJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJ3ZWJzdW1taXQiLCJhZG1pbiI6dHJ1ZSwic3ViIjoiMjFkN2JkYTEtOGZkNS00MjJiLWFlYjItOWQ4NDMzNjhkZTQ0IiwiYWRtaW5fZW1haWwiOiJ0b21pc2xhdi5zdmVjYWtAd2Vic3VtbWl0LmNvbSIsImNvbmZfaWQiOiJjM2E3ZjQ2Zi1kMjY5LTQ2ZDItYmUzMS0wNWNhNzczMzA4ODYiLCJjb25mX3NsdWciOiJ3czIwIn0.Bqc9CJCwwOjx9GrigMGjy397pK57gqf6DCJY4Ets3KY"}) => {
+const AuditTrail = ({bookingRef, conferenceSlug, token}: {bookingRef: string; conferenceSlug: string; token: string}) => {
   const {
     loading,
     error,
@@ -90,40 +89,17 @@ const AuditTrail = ({bookingRef = "CSRL-1", conferenceSlug = "ws20", token = "ey
       trails = trails.concat(assignmentTrailsVersions)
     }
     const orderedTrails = trails.sort((a,b) => (a.createdAt && b.createdAt && a.createdAt < b.createdAt) ? 1 : ((a.createdAt && b.createdAt && b.createdAt < a.createdAt) ? -1 : 0))
-    const Change = ({title, oldValue, newValue}:{title?: string; oldValue?:string, newValue?: string}) => {
-      return (
-        <div>
-          <div>{title}:</div>
-          <div>old value - {oldValue || 'undefined'}</div>
-          <div>new value - {newValue || 'undefined'}</div>
-        </div>
-      )
-    }
     return (
       <TrailsList>
         <Trail>
-          <Column>Created at</Column>
+          <MediumColumn>Created at</MediumColumn>
           <Column>Type</Column>
           <Column>Event</Column>
           <Column>Reason</Column>
           <MediumColumn>Who</MediumColumn>
-          <WideColumn>Changes</WideColumn>
+          <Column>Changes</Column>
         </Trail>
-        {orderedTrails.map(trail => {
-          const objectChanges = trail.objectChanges && JSON.parse(trail.objectChanges)
-          console.log(trail.event, objectChanges)
-          return (
-            <AuditTrailItem
-              itemId={trail.itemId}
-              createdAt={trail.createdAt}
-              itemType={trail.itemType}
-              event={trail.event}
-              reason={trail.reason}
-              whodunnit={trail.whodunnit}
-              objectChanges={objectChanges}
-            />
-          )
-        })}
+        {orderedTrails.map(trail => <AuditTrailItem key={trail.itemId} trail={trail} />)}
       </TrailsList>
     )}
   return <div>none</div>
