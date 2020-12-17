0a773dd1098ae11169b202ba3ba7dc09091e90a6
p-parafiniuk <pawel.parafiniuk@netguru.com>
2020-12-17 11:54:38 +0100

    feature(comp): adjust desing

--- frontends/ticket-support/src/components/order/OrderDetailsSummary.tsx
+++ frontends/ticket-support/src/components/order/OrderDetailsSummary.tsx
@@ -4,6 +4,7 @@ import styled from 'styled-components'
 import Loader from '../../lib/Loading'
 import Badge from '../shared/Badge'
 import Warning from '../ticketActions/Warning'
+import { Button } from '../ticketDetails/TicketDetails'
 import StatePlate from '../ticketItem/StatePlate'
 
 const StyledContainer = styled.section`
@@ -17,23 +18,43 @@ const StyledContainer = styled.section`
 
 const StyledFieldContainer = styled.div`
   display: flex;
+  align-items: center;
   font-size: 16px;
+
+  margin-bottom: 16px;
 `
 
 const StyledField = styled.input`
   display: flex;
   font-size: 16px;
+  border-radius: 8px;
+  border: 1px solid #999;
+  padding: 8px 16px;
 
   // TODO PP: Move to some utility (s)css class eg. spacing--md
+  margin-left: 16px;
   margin-right: 16px;
 `
-
-const StyledHeader = styled.div`
-  font-weight: 500;
-  font-size: 24px;
+const StyledDefaultContainer = styled.div`
+  display: flex;
+  flex-wrap: wrap;
 `
 
-const StyledInfoHeader = styled.div`
+const StyledCenteredContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+`
+
+// Headers
+const StyledHeader = styled.div`
+  font-weight: bold;
+  font-size: 18px;
+
+  // TODO PP: spacing needs to be evaluated as modifier to box
+  margin-bottom: 16px;
+`
+
+const StyledInfoHeader = styled.span`
   font-weight: 500;
   font-size: 18px;
   color: #97b5e8; // TODO: add color constants, needs to be discussed
@@ -41,11 +62,6 @@ const StyledInfoHeader = styled.div`
   // TODO PP: Move to some utility (s)css class eg. spacing--md
   // Cause: spacing needs to be evaluated as modifier to box otherwise style lost its ability to be reusable
   margin-bottom: 16px;
-`
-
-const StyledDefaultContainer = styled.div`
-  display: flex;
-  flex-wrap: wrap;
 `
 
 export type OrderDetailsSummaryProps = {
@@ -80,22 +96,30 @@ const OrderDetailsSummary = ({
             <StyledDefaultContainer>
               <StyledFieldContainer>
                 <span>Order Reference #</span>
-                <StyledField type="text" disabled value={orderReference} />
+                <StyledField disabled type="text" value={orderReference} />
               </StyledFieldContainer>
+            </StyledDefaultContainer>
 
+            <StyledDefaultContainer>
               <StyledFieldContainer>
                 <span>Created On:</span>
-                <input type="text" disabled value={orderDetails.createdOn.value} />
+                <StyledField
+                  disabled
+                  type="text"
+                  value={formatDefaultDate(orderDetails.createdOn.value)}
+                />
               </StyledFieldContainer>
 
               <StyledFieldContainer>
                 <span>Last Updated On:</span>
-                <input type="text" disabled value={orderDetails.lastUpdatedOn.value} />
+                <StyledField disabled type="text" value={orderDetails.lastUpdatedOn.value} />
               </StyledFieldContainer>
+            </StyledDefaultContainer>
 
+            <StyledDefaultContainer>
               <StyledFieldContainer>
                 <span>Source of Sale:</span>
-                <input type="text" disabled value={orderDetails.sourceOfSale.value} />
+                <StyledField disabled type="text" value={orderDetails.sourceOfSale.value} />
               </StyledFieldContainer>
 
               <StyledFieldContainer>
@@ -105,27 +129,33 @@ const OrderDetailsSummary = ({
               </StyledFieldContainer>
             </StyledDefaultContainer>
 
-            <StyledInfoHeader>
-              Order Owner’s Details ( Changes to this section shall transfer ticket assignment
-              ownership)
-            </StyledInfoHeader>
+            <StyledHeader>
+              Order Owner’s Details&nbsp;
+              <StyledInfoHeader>
+                (Changes to this section shall transfer ticket assignment ownership)
+              </StyledInfoHeader>
+            </StyledHeader>
 
             <StyledDefaultContainer>
               <StyledFieldContainer>
                 <span>First Name*:</span>
-                <input type="text" disabled value={orderDetails.name.value} />
+                <StyledField disabled type="text" value={orderDetails.name.value} />
               </StyledFieldContainer>
 
               <StyledFieldContainer>
                 <span>Last Name:</span>
-                <input type="text" disabled value={orderDetails.surname.value} />
-              </StyledFieldContainer>
-
-              <StyledFieldContainer>
-                <span>Email:</span>
-                <input type="email" disabled value={orderDetails.email.value} />
+                <StyledField disabled type="text" value={orderDetails.surname.value} />
               </StyledFieldContainer>
             </StyledDefaultContainer>
+
+            <StyledCenteredContainer>
+              <StyledFieldContainer>
+                <span>Email:</span>
+                <StyledField disabled type="email" value={orderDetails.email.value} />
+              </StyledFieldContainer>
+
+              <Button>Edit</Button>
+            </StyledCenteredContainer>
           </>
         )}
       </StyledContainer>
