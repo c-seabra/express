95f37693bf47a3d62b1b35dddb13811000824211
p-parafiniuk <pawel.parafiniuk@netguru.com>
2021-01-07 16:28:39 +0100

    feature(comp): TKT-812 adjust css values to be per design

--- frontends/ticket-support/src/lib/components/atoms/Badge.tsx
+++ frontends/ticket-support/src/lib/components/atoms/Badge.tsx
@@ -3,8 +3,9 @@ import styled from 'styled-components'
 export const Badge = styled.span`
   font-size: 14px;
   line-height: 24px;
-  padding: 4px 12px;
+  padding: 8px 16px;
   color: #fff;
   background-color: #000;
   border-radius: 4px;
+  min-width: 75px;
 `
