3644580addf2f689a257b91f18272262c923fcda
p-parafiniuk <pawel.parafiniuk@netguru.com>
2021-01-08 14:29:16 +0100

    feature(comp): TKT-810 remove token damnit

--- frontends/ticket-support/src/WebSummit-ticket-support.tsx
+++ frontends/ticket-support/src/WebSummit-ticket-support.tsx
@@ -9,6 +9,7 @@ import App from './components/app/App'
 const lifecycles = singleSpaReact({
   React,
   ReactDOM,
-  rootComponent: props => <App {...props} token="eyJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJ3ZWJzdW1taXQiLCJhZG1pbiI6dHJ1ZSwic3ViIjoiZjNjZGYwNTEtMGQzNy00Mjk5LWI0NmItMWJiNDQyMmY0ZDJjIiwiYWRtaW5fZW1haWwiOiJwYXdlbC5wYXJhZmluaXVrQG5ldGd1cnUuY29tIiwiY29uZl9pZCI6ImMzYTdmNDZmLWQyNjktNDZkMi1iZTMxLTA1Y2E3NzMzMDg4NiIsImNvbmZfc2x1ZyI6IndzMjAifQ.9YBHDBYDNS3qd76s7NaOdyK3AUjDHe9vKv5EYyjR_A8" />,})
+  rootComponent: props => <App {...props} token="12345" />,
+})
 
 export const { bootstrap, mount, unmount, update } = lifecycles
