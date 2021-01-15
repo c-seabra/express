6c0722292834ef9e81e238e82fbefe521d8fd78b
p-parafiniuk <pawel.parafiniuk@netguru.com>
2021-01-15 12:33:19 +0100

    feature(comp): TKT-815 Introduce 2nd level content navbar

--- frontends/ticket-support/src/lib/components/molecules/MainNavigation.tsx
+++ frontends/ticket-support/src/lib/components/molecules/MainNavigation.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { ReactElement } from 'react'
 import styled from 'styled-components'
 
 import { Route, Routes } from '../../constants/routes'
@@ -18,29 +18,77 @@ const StyledNavigation = styled.nav`
     line-height: 24px;
     font-weight: 300;
     text-decoration: none;
+    cursor: pointer;
+
+    &:hover {
+      color: #2269bf;
+      transition: all 0.3s ease-out;
+    }
 
     &.active {
       color: #0067e9;
+      font-weight: 600;
+    }
+
+    &.inactive {
+      color: gray;
       font-weight: 600;
     }
   }
 
   li {
     display: flex;
+
+    &:hover {
+      > .nav_submenu {
+        display: block;
+      }
+    }
+  }
+`
+const StyledDropbox = styled.div`
+  display: none;
+  position: absolute;
+  top: 45px;
+  border: 1px solid #dbdfe6;
+  background-color: white;
+  border-radius: 2px;
+
+  li {
+    padding: 4px 10px;
+
+    &:hover {
+      background-color: #f2f3f6;
+      color: #0c1439;
+      font-size: 14px;
+      letter-spacing: 0;
+      line-height: 30px;
+      cursor: pointer;
+    }
   }
 `
 
 type Props = {
-  routes: Routes
+  children: ReactElement
+  routes?: Routes
 }
 
-const MainNavigation = ({ routes }: Props) => {
+const MainNavigation = ({ routes, children }: Props) => {
   return (
     <StyledNavigation>
       {routes &&
         Object.values(routes).map((route: Route) => (
           <li key={route.path}>
             <a>{route?.meta?.description}</a>
+            <StyledDropbox className="nav_submenu">
+              {route.children &&
+                route.children?.length > 0 &&
+                route.children?.map((childRoute: Route) => (
+                  <li key={childRoute.path}>
+                    <a>{childRoute?.meta?.description}</a>
+                  </li>
+                ))}
+            </StyledDropbox>
             {route.hasChildren && <span className="material-icons">keyboard_arrow_down</span>}
           </li>
         ))}
