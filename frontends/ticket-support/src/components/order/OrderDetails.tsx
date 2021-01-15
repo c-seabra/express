48805da4478689e2cbbd59007f57ee90236c52a7
p-parafiniuk <pawel.parafiniuk@netguru.com>
2021-01-15 15:02:23 +0100

    change label for no data

--- frontends/ticket-support/src/components/order/OrderDetails.tsx
+++ frontends/ticket-support/src/components/order/OrderDetails.tsx
@@ -108,7 +108,7 @@ const OrderDetails: React.FC = () => {
   const order = data?.order
   const tickets = order?.tickets
   const owner = order?.owner
-  const missingDataAbbr = 'No data'
+  const missingDataAbbr = 'N/A'
 
   const { loading: mockedLoading, error: mockedError, orderDetails, orderSummary } = {
     error: false,
