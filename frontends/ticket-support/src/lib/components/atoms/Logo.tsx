6f728e59d522a72fc1082a1b4f3a10c190fd3415
p-parafiniuk <pawel.parafiniuk@netguru.com>
2021-01-18 15:44:44 +0100

    feature(comp): introduce logo component without asset

---
+++ frontends/ticket-support/src/lib/components/atoms/Logo.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+// import logo from './logo.svg'
+
+// const Logo = props => <img alt="Logo" {...props} src={logo} />
+const Logo = props => <img alt="Ticket Machine Logo" {...props} />
+
+export default Logo
