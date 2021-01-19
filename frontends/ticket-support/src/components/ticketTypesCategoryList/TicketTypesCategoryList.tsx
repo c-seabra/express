1083502328f66eefce3f15aa260bfcc77b3df87d
kcygan <kacper.cygan@netguru.com>
2021-01-19 13:25:24 +0100

    Added new filter design to ticket dashboard

---
+++ frontends/ticket-support/src/components/ticketTypesCategoryList/TicketTypesCategoryList.tsx
@@ -0,0 +1,35 @@
+import React, { ReactElement } from 'react'
+
+import CategoryList from '../../lib/components/molecules/CategoryList'
+import useTicketTypesCategoryList from '../../lib/hooks/useTicketTypesCategoryList'
+import { TicketType } from '../../lib/types'
+
+type TicketTypesCategoryListProps = {
+  initialValues?: string[]
+  onTicketTypeFilterChange?: (selectedTypes: string[]) => void
+  ticketTypes?: TicketType[]
+}
+
+const TicketTypesCategoryList = ({
+  initialValues = [],
+  onTicketTypeFilterChange = () => null,
+  ticketTypes,
+}: TicketTypesCategoryListProps): ReactElement => {
+  const { ticketTypesOptions, handleTicketTypeFilterChange } = useTicketTypesCategoryList({
+    initialValues,
+    onTicketTypeFilterChange,
+    ticketTypes,
+  })
+
+  return (
+    <CategoryList
+      isMultiSelect
+      headerColor="#654DA0"
+      items={ticketTypesOptions}
+      title="Ticket type"
+      onMultiClick={handleTicketTypeFilterChange}
+    />
+  )
+}
+
+export default TicketTypesCategoryList
---
+++ frontends/ticket-support/src/lib/hooks/useOrdersQuery.ts
@@ -0,0 +1,47 @@
+import { useAppContext } from '../../components/app/AppContext'
+import ORDER_LIST from '../../operations/queries/OrderList'
+import { Order } from '../types'
+import usePaginatedQuery from './usePaginatedQuery'
+
+const ORDERS_PER_PAGE = 20
+
+const useOrdersQuery = ({
+  initialPage,
+  perPage = ORDERS_PER_PAGE,
+  searchQuery,
+  status,
+  ticketTypeIds = [],
+}: {
+  initialPage: string
+  perPage?: number
+  searchQuery?: string
+  status?: string
+  ticketTypeIds?: string[]
+}) => {
+  const { conferenceSlug, token } = useAppContext()
+
+  const context = {
+    slug: conferenceSlug,
+    token,
+  }
+
+  const filter = {
+    status,
+    ticketTypeIds: ticketTypeIds?.length > 0 ? ticketTypeIds : undefined,
+  }
+
+  const variables = {
+    filter,
+    first: perPage,
+    searchQuery,
+  }
+
+  return usePaginatedQuery<Order, 'orders', typeof variables, typeof context>({
+    context,
+    initialPage,
+    query: ORDER_LIST,
+    variables,
+  })
+}
+
+export default useOrdersQuery
