3a3e5555400f0811388377757884c66523c656c8
Tom Svecak <tsvecak@gmail.com>
2021-01-22 14:48:59 +0000

    Minor updates to fix some errors

--- frontends/investor-meetings/src/components/app/App.tsx
+++ frontends/investor-meetings/src/components/app/App.tsx
@@ -4,21 +4,19 @@ import React, { useEffect, useState } from 'react'
 import withApollo from '../../lib/apollo/withApollo'
 
 const App = ({ token }: { token: string }) => {
-  // if (!token) return null
-  // const tokenPayload: { conf_slug: string; email: string } = jwt(token) as {
-  //   conf_slug: string
-  //   email: string
-  // }
+  const [conferenceSlug, setConferenceSlug] = useState<string>()
+  if (!token) return null
+  const tokenPayload: { conf_slug: string; email: string } = jwt(token) as {
+    conf_slug: string
+    email: string
+  }
 
-  // useEffect(() => {
-  //   setConferenceSlug(tokenPayload.conf_slug)
-  // }, [token])
+  useEffect(() => {
+    setConferenceSlug(tokenPayload.conf_slug)
+  }, [token, tokenPayload.conf_slug])
 
-  // const [conferenceSlug, setConferenceSlug] = useState<string>()
 
-  return (
-    <div>Starter app</div>
-  )
+  return <div>{conferenceSlug} Starter app</div>
 }
 
 export default withApollo(App)
