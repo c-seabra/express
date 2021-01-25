186f290565f588128647fef4fc61f443edabf053
p-parafiniuk <pawel.parafiniuk@netguru.com>
2021-01-25 14:24:46 +0100

    feature(comp): TKT-876 Adjust row desing for details

--- frontends/ticket-support/src/components/auditTrail/AuditTrailItem.tsx
+++ frontends/ticket-support/src/components/auditTrail/AuditTrailItem.tsx
@@ -1,12 +1,17 @@
 import React, { useState } from 'react'
 import styled from 'styled-components'
 
-import { Column, MediumColumn, TrailVersion, WideColumn } from './AuditTrail'
+import { Column, TrailVersion, WideColumn } from './AuditTrail'
 
 const DataRow = styled.div`
   display: flex;
   padding: 1rem 0.75rem;
   border-bottom: 1px solid #dcdfe5;
+
+  color: #0c1439;
+  font-size: 14px;
+  letter-spacing: 0;
+  line-height: 24px;
 `
 
 const DetailsRow = styled.div`
@@ -15,17 +20,50 @@ const DetailsRow = styled.div`
   background-color: #f7f9fa;
 `
 
+const DetailsContainer = styled.div`
+  display: flex;
+`
+
+const DetailContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  margin-bottom: 16px;
+`
+
+const DetailValue = styled.span`
+  color: #0c1439;
+  font-size: 14px;
+  letter-spacing: 0;
+  line-height: 24px;
+
+  margin-right: 100px;
+
+  &:last-child {
+    margin-right: 0;
+  }
+`
+
+const DetailLabel = styled(DetailValue)`
+  font-weight: 600;
+`
+
+const DetailLabelCapitalized = styled(DetailLabel)`
+  text-transform: capitalize;
+`
+
 const Change = ({ title, values }: { title?: string; values: Array<string> | unknown }) => {
   const val = Array.isArray(values) ? (values as Array<string>) : values
   const oldVal: string = Array.isArray(val) ? val?.[0] : ''
   const newVal: string = Array.isArray(val) ? val?.[1] : ''
+  const noDataLabel = 'undefined'
+
   return (
-    <div>
-      <div>{title}</div>
-      <div>old value - {oldVal || 'undefined'}</div>
-      <div>new value - {newVal || 'undefined'}</div>
-      <hr />
-    </div>
+    <DetailContainer>
+      <DetailLabelCapitalized>{title}</DetailLabelCapitalized>
+      <DetailValue>old value - {oldVal || noDataLabel}</DetailValue>
+      <DetailValue>new value - {newVal || noDataLabel}</DetailValue>
+    </DetailContainer>
   )
 }
 
@@ -60,17 +98,18 @@ const AuditTrailItem = ({
             return <Change title={key} values={value} />
           })}
           {context?.assignments && (
-            <>
-              <div>
+            <DetailsContainer>
+              <DetailContainer>
                 Assignee name - {context.assignments.current?.assignee_name || 'undefined'}
                 <br />
                 Assignee email - {context.assignments.current?.assignee_email || 'undefined'}
-              </div>
-              <div>
+              </DetailContainer>
+              <DetailContainer>
                 Assigner name - {context.assignments.current?.assigner_name || 'undefined'}
                 <br />
                 Assigner email - {context.assignments.current?.assigner_email || 'undefined'}
-              </div>
+              </DetailContainer>
+
               <div>
                 Previous Assignee name -{' '}
                 {context.assignments.previous?.assignee_name || 'undefined'}
@@ -85,7 +124,7 @@ const AuditTrailItem = ({
                 Previous Assigner email -{' '}
                 {context.assignments.previous?.assigner_email || 'undefined'}
               </div>
-            </>
+            </DetailsContainer>
           )}
           {context?.assigne && (
             <div>
