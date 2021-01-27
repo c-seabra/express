fe2b9e855b4e31b8ac37abc183ebd5e58d38c5c1
kcygan <kacper.cygan@netguru.com>
2021-01-27 16:16:22 +0100

    Created an input field to be used with formik

---
+++ frontends/ticket-support/src/lib/components/molecules/TextInputField.tsx
@@ -0,0 +1,66 @@
+import { ErrorMessage, Field, FieldProps } from 'formik'
+import React, { HTMLProps } from 'react'
+import styled, { css } from 'styled-components'
+
+const FieldContainer = styled.div`
+  font-family: 'azo-sans-web', sans-serif;
+  display: flex;
+  flex-direction: column;
+  min-height: 85px;
+`
+
+const Label = styled.div<{ required?: boolean }>`
+  color: #091a46;
+  font-size: 14px;
+  margin-bottom: 4px;
+
+  ${props =>
+    props.required &&
+    css`
+      &:after {
+        color: #e15554;
+        content: '*';
+      }
+    `}
+`
+const StyledInput = styled.input<{ isError?: boolean }>`
+  font-size: 14px;
+  font-weight: 300;
+  border: 1px solid ${props => (props.isError ? '#e15554' : '#dcdfe5')};
+  border-radius: 4px;
+  min-height: 40px;
+  padding-left: 1rem;
+  box-sizing: border-box;
+  width: 100%;
+  color: #07143e;
+`
+
+const Error = styled.div`
+  color: #e15554;
+  font-size: 12px;
+  margin-top: 4px;
+`
+
+type TextInputFieldProps = HTMLProps<HTMLInputElement> & {
+  name: string
+}
+
+const TextInputField = ({ className, label, name, required, placeholder }: TextInputFieldProps) => {
+  return (
+    <FieldContainer>
+      {label && <Label required={required}>{label}</Label>}
+      <Field className={className} name={name} required={required} type="text">
+        {({ meta, field }: FieldProps) => (
+          <StyledInput
+            isError={meta.touched && !!meta.error}
+            {...field}
+            placeholder={placeholder}
+          />
+        )}
+      </Field>
+      <ErrorMessage name={name} render={message => <Error>{message}</Error>} />
+    </FieldContainer>
+  )
+}
+
+export default TextInputField
