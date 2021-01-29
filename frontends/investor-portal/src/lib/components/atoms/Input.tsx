91e7c53e6c3490693af8ec49c3e52a58b000c7f9
Wesley Farrell <wesfarreller@gmail.com>
2021-01-29 09:54:01 +0000

    add routes, component files

---
+++ frontends/investor-portal/src/lib/components/atoms/Input.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const StyledInput = styled.input`
+  display: flex;
+  font-size: 16px;
+  border-radius: 2px;
+  border: 1px solid #a9a9a9;
+  padding: 0.5rem 0.5rem;
+
+  &:disabled {
+    color: #9ba5ae;
+  }
+`
+
+type Props = React.InputHTMLAttributes<HTMLInputElement>
+
+export const Input = ({ ...props }: Props) => {
+  return <StyledInput {...props} />
+}
---
+++ frontends/investor-portal/src/lib/components/index.ts
@@ -0,0 +1,9 @@
+/**
+ * Main components exports
+ *
+ * Note: More advance import solution could be provided e.g. https://github.com/diegohaz/arc/blob/master/src-example/components/index.js
+ */
+
+export { Button } from './atoms/Button'
+export { Input } from './atoms/Input'
+export { Label } from './atoms/Label'
