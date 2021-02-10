140701e8354b6cfda35a626bea5c2b7eb6cddee8
Alex Anikeev <alex.anikeev@websummit.com>
2021-02-10 17:34:58 +0700

    Implemented generic modal

--- frontends/investor-portal/src/lib/components/molecules/Modal.tsx
+++ frontends/investor-portal/src/lib/components/molecules/Modal.tsx
@@ -1,57 +1,139 @@
-import React, { ReactElement } from 'react'
+import React, { ReactElement, useState } from 'react'
 import ReactModal from 'react-modal'
-import styled from 'styled-components'
 
-const customStyles = {
-  content: {
-    bottom: 'auto',
-    left: '50%',
-    marginRight: '-50%',
-    right: 'auto',
-    top: '50%',
-    transform: 'translate(-50%, -50%)',
-  },
-  overlay: {
-    backgroundColor: 'rgba(0, 0, 0, 0.6)',
-  },
+import { Button, DestructiveButton, SecondaryButton } from '../atoms/Button'
+import Icon from '../atoms/Icon'
+import {
+  DefaultFooterSpacer,
+  ExitActionContainer,
+  ModalContainer,
+  ModalDescription,
+  ModalFooter,
+  ModalHeader,
+  ModalTitle,
+} from './Modal.styled'
+
+export const useModalState = ({ initialState = false }: { initialState?: boolean } = {}) => {
+  const [isOpen, setOpen] = useState(initialState)
+
+  return {
+    closeModal: () => setOpen(false),
+    isOpen,
+    openModal: () => setOpen(true),
+    toggleModal: () => setOpen(prevState => !prevState),
+  }
 }
 
-const ModalContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-`
-
-const ExitActionContainer = styled.div`
-  display: flex;
-  justify-content: flex-end;
-`
-
-const ExitIcon = styled.span`
-  cursor: pointer;
-`
-
-type ModalProps = {
+export type ModalProps = {
   children?: ReactElement | ReactElement[] | string
+  title?: string
+  description?: string
   isOpen: boolean
+  noPadding?: boolean
   onRequestClose: () => void
+  renderFooter?: () => ReactElement
+  withDefaultFooter?: boolean
+  defaultFooterIsDestructive?: boolean
+  defaultFooterNegativeButtonAction?: () => void
+  defaultFooterNegativeButtonText?: string
+  defaultFooterPositiveButtonAction?: () => void
+  defaultFooterPositiveButtonText?: string
   withoutDefaultActions?: boolean
 }
 
-const Modal = ({ isOpen, onRequestClose, withoutDefaultActions = false, children }: ModalProps) => {
+type FooterProps = {
+  isDestructive: boolean
+  negativeButtonAction: () => void
+  negativeButtonText: string
+  positiveButtonAction: () => void
+  positiveButtonText: string
+}
+
+const DefaultModalFooter = ({
+  isDestructive,
+  positiveButtonText,
+  negativeButtonText,
+  positiveButtonAction,
+  negativeButtonAction,
+}: FooterProps) => (
+  <ModalFooter>
+    <SecondaryButton style={{ marginRight: 10 }} onClick={negativeButtonAction}>
+      {negativeButtonText}
+    </SecondaryButton>
+    {!isDestructive && (
+      <Button type="submit" onClick={positiveButtonAction}>
+        {positiveButtonText}
+      </Button>
+    )}
+    {isDestructive && (
+      <DestructiveButton type="submit" onClick={positiveButtonAction}>
+        {positiveButtonText}
+      </DestructiveButton>
+    )}
+  </ModalFooter>
+)
+
+const Modal = ({
+  title,
+  description,
+  isOpen,
+  onRequestClose,
+  withoutDefaultActions = false,
+  noPadding = false,
+  children,
+  renderFooter,
+  withDefaultFooter = false,
+  defaultFooterIsDestructive = false,
+  defaultFooterPositiveButtonText = 'Submit',
+  defaultFooterNegativeButtonText = 'Cancel',
+  defaultFooterPositiveButtonAction = onRequestClose,
+  defaultFooterNegativeButtonAction = onRequestClose,
+}: ModalProps) => {
+  const customStyles = {
+    content: {
+      bottom: 'auto',
+      flex: 1,
+      left: '50%',
+      marginRight: '-50%',
+      padding: noPadding ? '0' : '16px',
+      right: 'auto',
+      top: '50%',
+      transform: 'translate(-50%, -50%)',
+    },
+    overlay: {
+      backgroundColor: 'rgba(0, 0, 0, 0.6)',
+    },
+  }
+
   return (
     <ReactModal isOpen={isOpen} style={customStyles} onRequestClose={onRequestClose}>
       <ModalContainer>
-        {!withoutDefaultActions && (
-          <ExitActionContainer>
-            <ExitIcon className="material-icons" onClick={onRequestClose}>
-              close
-            </ExitIcon>
-          </ExitActionContainer>
-        )}
+        <ModalHeader title={title}>
+          {title && <ModalTitle>{title}</ModalTitle>}
+          {!withoutDefaultActions && (
+            <ExitActionContainer noPadding={noPadding}>
+              <Icon onClick={onRequestClose}>close</Icon>
+            </ExitActionContainer>
+          )}
+        </ModalHeader>
+        {description && <ModalDescription>{description}</ModalDescription>}
         {children}
+        {withDefaultFooter && <DefaultFooterSpacer />}
+        {!withDefaultFooter && renderFooter && renderFooter()}
+        {withDefaultFooter && (
+          <Modal.DefaultFooter
+            isDestructive={defaultFooterIsDestructive}
+            negativeButtonAction={defaultFooterNegativeButtonAction}
+            negativeButtonText={defaultFooterNegativeButtonText}
+            positiveButtonAction={defaultFooterPositiveButtonAction}
+            positiveButtonText={defaultFooterPositiveButtonText}
+          />
+        )}
       </ModalContainer>
     </ReactModal>
   )
 }
+
+Modal.DefaultFooter = DefaultModalFooter
 
 export default Modal
