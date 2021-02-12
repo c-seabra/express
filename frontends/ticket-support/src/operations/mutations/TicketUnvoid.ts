056fd05302e3200c2cc5ce8d39508f73fb0f8825
p-parafiniuk <pawel.parafiniuk@netguru.com>
2021-02-12 12:46:09 +0100

    feature(comp): TKT-930 add new unvoid mutation

---
+++ frontends/ticket-support/src/operations/mutations/TicketUnvoid.ts
@@ -0,0 +1,73 @@
+import { gql, useMutation } from '@apollo/client'
+import { useState } from 'react'
+
+import { useAppContext } from '../../components/app/AppContext'
+import { useErrorSnackbar, useSuccessSnackbar } from '../../lib/hooks/useSnackbarMessage'
+import { Ticket, UserError } from '../../lib/types'
+
+export const TICKET_UNVOID_MUTATION = gql`
+  mutation UnvoidTicket($input: TicketUnvoidInput!) {
+    ticketUnvoid(input: $input) {
+      ticket {
+        id
+        state
+        bookingRef
+      }
+      userErrors {
+        message
+        path
+      }
+    }
+  }
+`
+
+type TicketUnvoidResponse = {
+  response: {
+    ticket: Ticket
+    userErrors: UserError[]
+  }
+}
+
+type TicketsUnvoidArgs = {
+  bookingRef: string
+  reason: string
+}
+
+export const useTicketUnvoidMutation = () => {
+  const { conferenceSlug, token } = useAppContext()
+  const [error, setError] = useState('')
+  const snackbar = useSuccessSnackbar()
+  const errSnackbar = useErrorSnackbar()
+
+  const [voidTicketMutation] = useMutation<TicketUnvoidResponse>(TICKET_UNVOID_MUTATION, {
+    onCompleted: ({ response }) => {
+      snackbar('Ticket unvoided')
+
+      if (response?.userErrors.length) {
+        setError(response.userErrors[0]?.message)
+        errSnackbar('Ticket unvoiding failed')
+      }
+    },
+    refetchQueries: ['TicketAuditTrail', 'Ticket'],
+  })
+
+  const unvoidTicket = async ({ reason, bookingRef }: TicketsUnvoidArgs) => {
+    await voidTicketMutation({
+      context: {
+        headers: {
+          'x-admin-reason': reason,
+        },
+        slug: conferenceSlug,
+        token,
+      },
+      variables: {
+        input: { reference: bookingRef },
+      },
+    })
+  }
+
+  return {
+    error,
+    unvoidTicket,
+  }
+}
