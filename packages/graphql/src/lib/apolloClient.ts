68c8649ba8ab0a50ef08e11ebaf4ae95d96d6cdf
p-parafiniuk <pawel.parafiniuk@netguru.com>
2021-02-17 17:07:51 +0100

    feature(apollo): format code

--- packages/graphql/src/lib/apolloClient.ts
+++ packages/graphql/src/lib/apolloClient.ts
@@ -39,11 +39,14 @@ const createApolloClient = (apiURL: string) => {
     return forward(operation);
   });
 
-  const httpLink = new HttpLink({ fetch, uri: ({ operationName }) => {
-    const shortPath = `${apiURL}`
-    const fullPath = `${apiURL}/${operationName}`
+  const httpLink = new HttpLink({
+    fetch,
+    uri: ({ operationName }) => {
+      const shortPath = `${apiURL}`;
+      const fullPath = `${apiURL}/${operationName}`;
       return operationName ? fullPath : shortPath;
-    } });
+    },
+  });
 
   const links = [setHeadersLink, httpLink];
   const link = ApolloLink.from(links);
