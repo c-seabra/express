e1273c98b9fc99649d8780d637005bd8f2b856a9
p-parafiniuk <pawel.parafiniuk@netguru.com>
2021-02-18 17:34:56 +0100

    feature(comp): TKT-977 introduce void mutation in lib

---
+++ packages/graphql/src/operations/mutations/TicketVoidMutation.ts
@@ -0,0 +1,18 @@
+import { gql } from '@apollo/client';
+
+export const TICKET_VOID_MUTATION = gql`
+  mutation VoidTicket($input: TicketVoidInput!) {
+    ticketVoid(input: $input) {
+      ticket {
+        id
+        state
+        bookingRef
+      }
+      userErrors {
+        message
+        path
+      }
+    }
+  }
+`
+export default TICKET_VOID_MUTATION;
