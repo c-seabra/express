685f3b571367b1f869daf48d5db76861a27ca68a
Wesley Farrell <wesfarreller@gmail.com>
2021-02-23 15:49:54 +0000

    add SessionTimeslotParticipation mutation

--- frontends/investor-portal/src/lib/hooks/index.ts
+++ frontends/investor-portal/src/lib/hooks/index.ts
@@ -3,3 +3,4 @@ export { default as useEventQuery } from './useEventQuery';
 export { default as useAttendanceAppearanceSelectionDestroyMutation } from './useAttendanceAppearanceSelectionDestroyMutation';
 export { default as useEventUpdateMutation } from './useEventUpdateMutation';
 export { default as useInvestorSessionCreateMutation } from './useInvestorSessionCreateMutation';
+export { default as useSessionTimeslotParticipationCreateMutation } from './useSessionTimeslotParticipationCreateMutation';
---
+++ frontends/investor-portal/src/lib/hooks/useSessionTimeslotParticipationCreateMutation.ts
@@ -0,0 +1,70 @@
+import 'moment-timezone';
+
+import { useMutation } from '@apollo/client';
+
+import { useAppContext } from '../../components/app/AppContext';
+import SESSION_TIMESLOT_PARTICIPATION_CREATE from '../../operations/mutations/SessionTimeslotParticipationCreate';
+import { Attendance, UserError } from '../types';
+import { useErrorSnackbar, useSuccessSnackbar } from './useSnackbarMessage';
+
+type SessionTimeslotParticipationCreateData = {
+  sessionTimeslotParticipationCreate: {
+    participations: [Attendance]
+    successMessage: string;
+    userErrors: UserError[];
+  };
+};
+
+type SessionTimeslotParticipationCreatesArgs = {
+  attendanceId: string | undefined;
+  sessionTimeslotId: string | undefined;
+};
+
+const useSessionTimeslotParticipationCreateMutation = ({
+  attendanceId,
+  sessionTimeslotId
+}: SessionTimeslotParticipationCreatesArgs) => {
+  const { conferenceSlug, token } = useAppContext();
+  const success = useSuccessSnackbar();
+  const errorMessage = useErrorSnackbar();
+
+  const [
+    createSessionTimeslotParticipation,
+    { data, error, loading },
+  ] = useMutation<SessionTimeslotParticipationCreateData>(
+    SESSION_TIMESLOT_PARTICIPATION_CREATE,
+    {
+      onCompleted: ({ sessionTimeslotParticipationCreate }) => {
+        if (sessionTimeslotParticipationCreate?.userErrors[0]) {
+          errorMessage(sessionTimeslotParticipationCreate?.userErrors[0].message);
+        } else {
+          success(sessionTimeslotParticipationCreate.successMessage);
+        }
+      },
+      onError: (e) => errorMessage(e.message),
+    },
+  );
+
+  const createSessionTimeslotParticipationMutation = async () => {
+    await createSessionTimeslotParticipation({
+      context: {
+        slug: conferenceSlug,
+        token,
+      },
+      refetchQueries: ['AttendanceAppearanceSelectionQuery'],
+      variables: {
+        attendanceId,
+        sessionTimeslotId
+      },
+    });
+  };
+
+  return {
+    createSessionTimeslotParticipationMutation,
+    data,
+    error,
+    loading,
+  };
+};
+
+export default useSessionTimeslotParticipationCreateMutation;
