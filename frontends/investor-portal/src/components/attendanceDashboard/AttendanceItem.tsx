0cdb956c253a3d6d963ffe5eb8c3c16de31836b0
Marek Parafianowicz <marek.parafianowicz@netguru.com>
2021-02-23 08:24:16 +0100

    Import styles from styled components

--- frontends/investor-portal/src/components/attendanceDashboard/AttendanceItem.tsx
+++ frontends/investor-portal/src/components/attendanceDashboard/AttendanceItem.tsx
@@ -1,24 +1,9 @@
 import React, { ReactElement } from 'react'
 import { useHistory } from 'react-router-dom'
-import styled from 'styled-components'
 
 import { ListItem } from '../../lib/components'
 import { Attendance } from '../../lib/types'
-
-const ColumnStyles = styled.div`
-  text-align: center;
-  display: flex;
-  align-items: center;
-  padding: 0 0.25rem;
-  word-break: break-word;
-`
-const Column = styled(ColumnStyles)`
-  width: 15%;
-`
-
-const NarrowColumn = styled(ColumnStyles)`
-  width: 3%;
-`
+import { Column, NarrowColumn } from './AttendanceListHeader.styled'
 
 const AttendanceItem = ({
   attendance,
