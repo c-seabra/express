06c3ab82b5663f12d3a9d9338cc64e7355330a62
p-parafiniuk <pawel.parafiniuk@netguru.com>
2021-02-24 15:37:23 +0100

    feature(comp): TKT-975 introduce new desing for modal

--- frontends/ticket-support/src/components/ticketActions/TicketAssignModal.tsx
+++ frontends/ticket-support/src/components/ticketActions/TicketAssignModal.tsx
@@ -9,12 +9,29 @@ import Modal from '../../lib/components/molecules/Modal';
 import TextInputField from '../../lib/components/molecules/TextInputField';
 import useAssignTicketMutation from '../../lib/hooks/useTicketAssignMutation';
 import { Ticket } from '../../lib/types';
+import { Spacing } from '../templates/Spacing';
+import Icon from '../../lib/components/atoms/Icon';
+import { DisabledButton, ErrorButton } from '../../lib/components/atoms/Button';
+import {
+  AlertText,
+  FieldWrapper,
+  HeaderText,
+  IconWrapper,
+  StyledActionRow,
+  Text,
+  Wrapper,
+} from '../../lib/components/molecules/ReasonAlertModal';
 
 const ContentContainer = styled.div`
   padding: 2rem 0;
   width: 450px;
   font-size: 0.85rem;
   font-weight: 400;
+`;
+
+const StyledRow = styled.div`
+  display: flex;
+  justify-content: space-between;
 `;
 
 const ConfirmationText = styled.div`
@@ -59,7 +76,7 @@ const TicketAssignModal = ({
   ticket,
 }: TicketAssignModalProps) => {
   const isAssigned = ticket.assignment !== null;
-  console.log(isAssigned)
+  console.log(isAssigned);
   const { assignTicket } = useAssignTicketMutation();
   const [isFirstStepFilled, setFirstStepFilled] = useState(false);
 
@@ -70,13 +87,7 @@ const TicketAssignModal = ({
   };
 
   return (
-    <Modal
-      key={isOpen.toString()}
-      withDefaultFooter
-      isOpen={isOpen}
-      title={ isAssigned ? 'Reassign ticket' : 'Assign ticket'}
-      onRequestClose={handleClose}
-    >
+    <Modal key={isOpen.toString()} isOpen={isOpen} onRequestClose={handleClose}>
       <ContentContainer>
         <Formik
           initialValues={{
@@ -100,56 +111,134 @@ const TicketAssignModal = ({
           }}
         >
           {({ values }) => (
-            <StyledForm>
-              {isFirstStepFilled ? (
-                <>
-                  <ConfirmationText>
-                    Are you sure you want to&nbsp;<span>{ isAssigned ? 'reassign' : 'assign'}</span>
-                    &nbsp;ticket&nbsp;
-                    <span>{ticket.bookingRef}</span>?
-                  </ConfirmationText>
-                  <TextInputField
-                    required
-                    label="Specify a reason for the reassignment"
-                    name="reason"
-                  />
-                  <CheckboxField
-                    label="Send email notification to new and old assignee"
-                    name="notify"
-                  />
-                  {values.notify && (
-                    <WarningMessage>
+            <Form>
+              <Wrapper>
+                <Spacing bottom="10px">
+                  <IconWrapper>
+                    <Icon>error</Icon>
+                  </IconWrapper>
+                </Spacing>
+
+                <HeaderText>Are you sure you want to assign ticket</HeaderText>
+
+                <Spacing bottom="40px">
+                  <AlertText>{ticket.bookingRef}</AlertText>
+                </Spacing>
+
+                <Spacing bottom="24px">
+                  <StyledRow>
+                    <TextInputField
+                      required
+                      label="First name"
+                      name="firstName"
+                      placeholder="John"
+                    />
+                    <TextInputField
+                      label="Last name"
+                      name="lastName"
+                      placeholder="Doe"
+                    />
+                  </StyledRow>
+                  <StyledRow>
+                    <TextInputField
+                      required
+                      label="Email"
+                      name="email"
+                      placeholder="john.doe@example.com"
+                    />
+                  </StyledRow>
+                </Spacing>
+
+                <Spacing bottom="40px" top="24px">
+                  {isAssigned ? (
+                    <Text>
                       Email notifications will be sent to the new assignee, old
                       assignee, and order owner
-                    </WarningMessage>
+                    </Text>
+                  ) : (
+                    <Text>
+                      Email notifications will be sent to the new assignee and
+                      order owner
+                    </Text>
                   )}
-                </>
-              ) : (
-                <>
-                  <TextInputField
-                    required
-                    label="First name"
-                    name="firstName"
-                    placeholder="John"
-                  />
-                  <TextInputField
-                    label="Last name"
-                    name="lastName"
-                    placeholder="Doe"
-                  />
-                  <TextInputField
-                    required
-                    label="Email"
-                    name="email"
-                    placeholder="john.doe@example.com"
-                  />
-                </>
-              )}
-              <Modal.DefaultFooter
-                submitText={isFirstStepFilled ? 'Confirm' : 'Reassign ticket'}
-                onCancelClick={handleClose}
-              />
-            </StyledForm>
+                </Spacing>
+
+                {isAssigned && (
+                  <>
+                    <Spacing bottom="8px" top="8px">
+                      <FieldWrapper
+                        required
+                        label="Please specify the reason for your actions"
+                        maxLength={255}
+                        name="reason"
+                      />
+                    </Spacing>
+                    <CheckboxField
+                      label="Send email notification to new and old assignee"
+                      name="notify"
+                    />
+                  </>
+                )}
+
+                <Spacing bottom="50px">
+                  <StyledActionRow>
+                    <DisabledButton onClick={closeModal}>Cancel</DisabledButton>
+                    <ErrorButton type="submit">Confirm</ErrorButton>
+                  </StyledActionRow>
+                </Spacing>
+              </Wrapper>
+            </Form>
+
+            // <StyledForm>
+            //   {isFirstStepFilled ? (
+            //     <>
+            //       <ConfirmationText>
+            //         Are you sure you want to&nbsp;<span>{ isAssigned ? 'reassign' : 'assign'}</span>
+            //         &nbsp;ticket&nbsp;
+            //         <span>{ticket.bookingRef}</span>?
+            //       </ConfirmationText>
+            //       <TextInputField
+            //         required
+            //         label="Specify a reason for the reassignment"
+            //         name="reason"
+            //       />
+            //       <CheckboxField
+            //         label="Send email notification to new and old assignee"
+            //         name="notify"
+            //       />
+            //       {values.notify && (
+            //         <WarningMessage>
+            //           Email notifications will be sent to the new assignee, old
+            //           assignee, and order owner
+            //         </WarningMessage>
+            //       )}
+            //     </>
+            //   ) : (
+            //     <>
+            //       <TextInputField
+            //         required
+            //         label="First name"
+            //         name="firstName"
+            //         placeholder="John"
+            //       />
+            //       <TextInputField
+            //         label="Last name"
+            //         name="lastName"
+            //         placeholder="Doe"
+            //       />
+            //       <TextInputField
+            //         required
+            //         label="Email"
+            //         name="email"
+            //         placeholder="john.doe@example.com"
+            //       />
+            //     </>
+            //   )}
+            //   <Modal.DefaultFooter
+            //     submitText={isFirstStepFilled ? 'Confirm' : 'Reassign ticket'}
+            //     onCancelClick={handleClose}
+            //   />
+            // </StyledForm>
           )}
         </Formik>
       </ContentContainer>
