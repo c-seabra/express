093bc292fee179e6f19aa0d9999e38c7ffb7bf56
p-parafiniuk <pawel.parafiniuk@netguru.com>
2021-03-01 14:43:44 +0100

    feature(comp): TKT-905 extend OrderOwnerDetails with action row

--- frontends/ticket-support/src/components/order/OrderOwnerDetails.tsx
+++ frontends/ticket-support/src/components/order/OrderOwnerDetails.tsx
@@ -1,10 +1,13 @@
 import React, { ReactElement } from 'react';
 import styled from 'styled-components';
 
+import { Button, SecondaryButton } from '../../lib/components/atoms/Button';
 import ContainerCard from '../../lib/components/atoms/ContainerCard';
 import LabeledInput from '../../lib/components/molecules/LabeledInput';
+import { Spacing } from '../../lib/components/templates/Spacing';
 
 type OrderOwnerDetailsProps = {
+  editModeOn?: boolean;
   email?: string;
   firstName?: string;
   lastName?: string;
@@ -22,18 +25,37 @@ const OwnerDetails = styled.div`
   }
 `;
 
+const StyledActions = styled(Spacing)`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: flex-end;
+`;
+
 const OrderOwnerDetails = ({
   firstName,
   lastName,
   email,
+  editModeOn = false,
 }: OrderOwnerDetailsProps): ReactElement => {
   return (
+    // <ContainerCard title="Owner details" renderActions={(props) => <Button {...props} />}>
     <ContainerCard title="Owner details">
-      <OwnerDetails>
-        <LabeledInput disabled label="First name" value={firstName} />
-        <LabeledInput disabled label="Last name" value={lastName} />
-        <LabeledInput disabled label="Email" value={email} />
-      </OwnerDetails>
+      <>
+        <OwnerDetails>
+          <LabeledInput disabled={!editModeOn} label="First name" value={firstName} />
+          <LabeledInput disabled={!editModeOn} label="Last name" value={lastName} />
+          <LabeledInput disabled={!editModeOn} label="Email" value={email} />
+        </OwnerDetails>
+
+        {editModeOn && (
+          <StyledActions top="32px">
+            <Spacing right="16px">
+              <SecondaryButton onClick={() => null}>Cancel</SecondaryButton>
+            </Spacing>
+            <Button onClick={() => null}>Save</Button>
+          </StyledActions>
+        )}
+      </>
     </ContainerCard>
   );
 };
