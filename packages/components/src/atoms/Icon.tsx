dc22df90d35e64e07f64c588b9c95b28a0a7e59f
p-parafiniuk <pawel.parafiniuk@netguru.com>
2021-03-05 15:53:47 +0100

    feature(comp): TKT-1068 Move icon to package

---
+++ packages/components/src/atoms/Icon.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import styled, { css } from 'styled-components';
+
+type Props = React.BaseHTMLAttributes<HTMLSpanElement> & {
+  color?: string;
+  size?: string;
+};
+
+const StyledIcon = styled.span<Props>`
+  cursor: pointer;
+
+  ${(props) =>
+    props.color &&
+    css`
+      color: ${props.color};
+    `}
+
+  ${(props) =>
+    props.size &&
+    css`
+      font-size: ${props.size} !important; // Need to override material-icons defaults
+    `}
+`;
+
+const Icon = ({ color, size, children, ...props }: Props) => {
+  return (
+    <StyledIcon className="material-icons" color={color} size={size} {...props}>
+      {children}
+    </StyledIcon>
+  );
+};
+
+export default Icon;
