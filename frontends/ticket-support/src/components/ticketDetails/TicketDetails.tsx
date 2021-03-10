d2c0902748395d1952da29dbf8b5a03ffcf27931
p-parafiniuk <pawel.parafiniuk@netguru.com>
2021-03-10 12:58:38 +0100

    fix: revert business logic

--- frontends/ticket-support/src/components/ticketDetails/TicketDetails.tsx
+++ frontends/ticket-support/src/components/ticketDetails/TicketDetails.tsx
@@ -384,7 +384,7 @@ const TicketDetails = (): ReactElement => {
                       <UpdateUniqueUserIdentifier
                         accountId={assignment.assignee.id}
                         email={assignment.assignee?.email}
-                        isDisabled={isTicketVoided}
+                        isDisabled={!isTicketVoided}
                       />
                     )}
 
@@ -392,7 +392,7 @@ const TicketDetails = (): ReactElement => {
                       <UpdateAppLoginEmail
                         bookingRef={bookingRef}
                         email={assignment?.appLoginEmail || assignee?.email}
-                        isDisabled={isTicketVoided}
+                        isDisabled={!isTicketVoided}
                       />
                     )}
 
