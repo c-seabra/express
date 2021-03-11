091b3064adef2b4f1ea9e8a5ffb4e18a6b15de8c
p-parafiniuk <pawel.parafiniuk@netguru.com>
2021-03-11 14:51:12 +0100

    feature(comp): TKT-1081 make EventList comp more dumb

--- frontends/events/src/components/eventList/EventList.tsx
+++ frontends/events/src/components/eventList/EventList.tsx
@@ -1,37 +1,14 @@
-import { ApolloError, useQuery } from '@apollo/client';
 import React from 'react';
 
 import Loader from '../../lib/Loading';
-import { Event } from '../../lib/types';
-import EVENT_LIST from '../../operations/queries/EventList';
-import { useAppContext } from '../app/AppContext';
 import EventItem, { EventListHeader } from '../eventItem/EventItem';
 
-const EventList: React.FC = () => {
-  const { conferenceSlug, token } = useAppContext();
-
-  const {
-    loading,
-    error,
-    data,
-  }: {
-    data?: {
-      events: {
-        edges: [
-          {
-            node: Event;
-          },
-        ];
-      };
-    };
-    error?: ApolloError;
-    loading?: boolean;
-  } = useQuery(EVENT_LIST, {
-    context: {
-      slug: conferenceSlug,
-      token,
-    },
-  });
+type EventListProps = {
+  error: any;
+  events: any,
+  loading: boolean;
+}
+const EventList = ({loading, error, events}: EventListProps) => {
 
   if (loading) {
     return <Loader />;
@@ -41,13 +18,13 @@ const EventList: React.FC = () => {
     return <>{error.message}</>;
   }
 
-  const events = data?.events.edges.map((node) => node.node);
+  const mappedEvents = events.edges.map((node) => node.node);
 
   return (
     <>
       <h4>Events</h4>
       <EventListHeader />
-      {events?.map((event) => (
+      {mappedEvents?.map((event) => (
         <EventItem
           key={event.id}
           country={event?.country}
