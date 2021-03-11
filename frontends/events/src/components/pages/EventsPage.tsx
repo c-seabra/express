2e6d19792ac70691ac7f770d64a2d6783fb64f9f
p-parafiniuk <pawel.parafiniuk@netguru.com>
2021-03-11 14:03:38 +0100

    feature(comp): TKT-1081 introduce EventsPage as parent container

---
+++ frontends/events/src/components/pages/EventsPage.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import EventList from '../eventList/EventList';
+
+const EventPage: React.FC = () => {
+  return (
+    <>
+      <p>events page</p>;
+      <EventList />
+    </>
+  );
+};
+
+export default EventPage;
