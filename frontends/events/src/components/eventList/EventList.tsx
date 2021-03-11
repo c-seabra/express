086740dc1fa5dbece5b559478831a5514a77f749
p-parafiniuk <pawel.parafiniuk@netguru.com>
2021-03-11 17:36:49 +0100

    feature(comp): TKT-1079 add Table

--- frontends/events/src/components/eventList/EventList.tsx
+++ frontends/events/src/components/eventList/EventList.tsx
@@ -1,15 +1,44 @@
 import React from 'react';
 
+import ContainerCard from '@websummit/components/src/molecules/ContainerCard';
+import Table, {
+  ColumnDescriptor,
+} from '@websummit/components/src/molecules/Table';
+import { formatDefaultDateTime } from '../../../../ticket-support/src/lib/utils/time';
 import Loader from '../../lib/Loading';
-import EventItem, { EventListHeader } from '../eventItem/EventItem';
 import { Event } from '../../lib/types';
 
 type EventListProps = {
   error: any;
-  events: any,
+  events: any;
   loading: boolean;
-}
-const EventList = ({loading, error, events}: EventListProps) => {
+};
+
+const EventList = ({ loading, error, events }: EventListProps) => {
+  const tableShape: ColumnDescriptor<Event>[] = [
+    {
+      header: 'Name',
+      renderCell: (event) => event.name,
+      width: '20%',
+    },
+    {
+      header: 'Start date',
+      renderCell: (event) => formatDefaultDateTime(event.startDate),
+      width: '20%',
+    },
+    {
+      header: 'End date',
+      renderCell: (event) => formatDefaultDateTime(event.endDate),
+    },
+    {
+      header: 'Location',
+      renderCell: (event) => event.country,
+    },
+    {
+      header: 'Created by',
+      renderCell: (event) => 'cby',
+    },
+  ];
 
   if (loading) {
     return <Loader />;
@@ -22,19 +51,9 @@ const EventList = ({loading, error, events}: EventListProps) => {
   return (
     <>
       <h4>All events</h4>
-      <EventListHeader />
-      {events?.map((event: Event) => (
-        <EventItem
-          key={event.id}
-          country={event?.country}
-          currency={event?.currency}
-          description={event?.description}
-          endDate={event?.endDate}
-          name={event.name}
-          slug={event?.slug}
-          startDate={event?.startDate}
-        />
-      ))}
+      <ContainerCard noPadding>
+        <Table<Event> items={events} tableShape={tableShape} />
+      </ContainerCard>
     </>
   );
 };
