f294c476331b4994ec994a546bb39385f7efcaec
Marvin Hofmann <644950+reggaemuffin@users.noreply.github.com>
2021-03-23 12:19:08 +0000

    webpack config ready

--- frontends/omnia-container/webpack.config.js
+++ frontends/omnia-container/webpack.config.js
@@ -1,8 +1,8 @@
 const { merge } = require('webpack-merge');
-const singleSpaDefaults = require('webpack-config-single-spa-ts');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { EnvironmentPlugin } = require('webpack');
 const dotenv = require('dotenv');
+const SystemJSPublicPathWebpackPlugin = require('systemjs-webpack-interop/SystemJSPublicPathWebpackPlugin');
 
 dotenv.config({
   path: '../../.env',
@@ -10,13 +10,47 @@ dotenv.config({
 
 module.exports = (webpackConfigEnv, argv) => {
   const orgName = 'websummit-micro';
-  const defaultConfig = singleSpaDefaults({
-    argv,
-    disableHtmlGeneration: true,
-    orgName,
-    projectName: 'omnia-container',
-    webpackConfigEnv,
-  });
+  // this is a hack to just get it to build somehow
+  // the html plugin is the interesting bit
+  const defaultConfig = {
+    mode: 'production',
+    entry: '/Users/marvin/repos/micro/frontends/omnia-container/sourcemap.json',
+    output: {
+      filename: 'sourcemap.json',
+      libraryTarget: 'system',
+      path: '/Users/marvin/repos/micro/frontends/omnia-container/dist',
+      uniqueName: 'omnia-container',
+      devtoolNamespace: 'omnia-container',
+      publicPath: '',
+    },
+    devServer: {
+      compress: true,
+      historyApiFallback: true,
+      headers: { 'Access-Control-Allow-Origin': '*' },
+      firewall: false,
+      client: { host: 'localhost' },
+    },
+    externals: ['single-spa', /^@websummit-micro\//],
+    plugins: [
+      new SystemJSPublicPathWebpackPlugin({
+        // optional: defaults to 1
+        // If you need the webpack public path to "chop off" some of the directories in the current module's url, you can specify a "root directory level". Note that the root directory level is read from right-to-left, with `1` indicating "current directory" and `2` indicating "up one directory":
+        rootDirectoryLevel: 1,
+      }),
+    ],
+    resolve: {
+      extensions: [
+        '.mjs',
+        '.js',
+        '.jsx',
+        '.wasm',
+        '.json',
+        '.ts',
+        '.tsx',
+        '.json',
+      ],
+    },
+  };
 
   return merge(defaultConfig, {
     // modify the webpack config however you'd like to by adding to this object
