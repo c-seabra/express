08e98f67821151429ee400b442a57e76e21b6eaa
kcygan <kacper.cygan@netguru.com>
2021-03-29 13:45:39 +0200

    Fixed hardcoded avenger

--- frontends/events/src/lib/utils/eventConfig.ts
+++ frontends/events/src/lib/utils/eventConfig.ts
@@ -37,7 +37,7 @@ const getConfigCompletion = (data?: EventQuery) => (
   }
 
   return {
-    missing: Object.assign([], eventCompletionRequirements.avenger),
+    missing: Object.assign([], eventCompletionRequirements[rule]),
     ready: false,
   };
 };
