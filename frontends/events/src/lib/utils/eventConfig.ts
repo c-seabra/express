2bb6a05bc7565a062e7de83a5ef5bf4574126f1d
kcygan <kacper.cygan@netguru.com>
2021-03-29 13:34:57 +0200

    Added event config completion validator utility

---
+++ frontends/events/src/lib/utils/eventConfig.ts
@@ -0,0 +1,53 @@
+import { EventQuery } from '@websummit/graphql/src/@types/operations';
+
+// If any rules for required fields (backend-wise) change
+// those field keys should be added here appropriately
+const eventCompletionRequirements = {
+  avenger: ['id', 'slug'] as const,
+  stores: ['slug', 'legalEntity'] as const,
+  'ticket-assignment': ['id'] as const,
+  'ticket-release-mgmt': ['id'] as const,
+};
+
+type CompletionInfo = {
+  missing: string[];
+  ready: boolean;
+};
+
+const getConfigCompletion = (data?: EventQuery) => (
+  rule: keyof typeof eventCompletionRequirements,
+) => {
+  if (data?.event) {
+    const { event } = data;
+
+    const configCompletion: CompletionInfo = {
+      missing: [],
+      ready: true,
+    };
+    eventCompletionRequirements[rule].forEach(
+      (requiredField: keyof typeof event) => {
+        if (!event[requiredField]) {
+          configCompletion.missing.push(requiredField);
+          configCompletion.ready = false;
+        }
+      },
+    );
+
+    return configCompletion;
+  }
+
+  return {
+    missing: Object.assign([], eventCompletionRequirements.avenger),
+    ready: false,
+  };
+};
+
+export const getServicesReadyForEvent = (data?: EventQuery) => {
+  const configCompletionFor = getConfigCompletion(data);
+  return {
+    avenger: configCompletionFor('avenger'),
+    stores: configCompletionFor('stores'),
+    'ticket-assignment': configCompletionFor('ticket-assignment'),
+    'ticket-release-mgmt': configCompletionFor('ticket-release-mgmt'),
+  };
+};
