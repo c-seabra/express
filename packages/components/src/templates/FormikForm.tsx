eb55bcbd0d7f650c07b635371c0243b93b1a7d6c
p-parafiniuk <pawel.parafiniuk@netguru.com>
2021-04-12 16:30:05 +0200

    feature(comp): TKT-1227 cleanup FormikForm

--- packages/components/src/templates/FormikForm.tsx
+++ packages/components/src/templates/FormikForm.tsx
@@ -1,6 +1,3 @@
-import { Card } from '@material-ui/core';
-import Modal from '@websummit/components/src/molecules/Modal';
-import { Spacing } from '@websummit/components/src/templates/Spacing';
 import { Form, Formik } from 'formik';
 import React from 'react';
 import styled from 'styled-components';
@@ -13,29 +10,6 @@ export const Wrapper = styled.div`
   padding: 0;
   font-size: 0.85rem;
   font-weight: 400;
-  min-width: 600px;
-`;
-
-export const Text = styled.div`
-  font-size: 14px;
-  letter-spacing: 0;
-  line-height: 28px;
-  max-width: 580px;
-  text-align: left;
-`;
-
-export const HeaderText = styled.div`
-  color: #0067e9;
-  font-size: 32px;
-  font-weight: 500;
-  letter-spacing: -0.5px;
-  line-height: 40px;
-`;
-
-export const FieldWrapper = styled.div`
-  min-width: 580px;
-  text-align: left;
-  padding: 0;
 `;
 
 type ModalProps = {
@@ -70,11 +44,11 @@ const FormikForm = ({
         <Form>
           <Wrapper>
             {children}
-            <Spacing top="48px">
-              <FieldWrapper>
-                <Modal.DefaultFooter submitText={submitText} />
-              </FieldWrapper>
-            </Spacing>
+            {/* <Spacing top="48px"> */}
+            {/*  <FieldWrapper> */}
+            {/*    <Modal.DefaultFooter submitText={submitText} /> */}
+            {/*  </FieldWrapper> */}
+            {/* </Spacing> */}
           </Wrapper>
         </Form>
       </Formik>
