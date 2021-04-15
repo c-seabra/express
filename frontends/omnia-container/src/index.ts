388bbb30c82f9080fb1ae7fbf1b1b9277d2f09a8
Chris Bowes <chris.bowes@websummit.com>
2021-04-15 14:09:55 +0200

    Moves the call to fonts and icon styles into index.ts to inject the links into the omnia wrapper. Adds minor style update to ticket-support micro to tidy the rendering within omnia. Refactors the injection of single-spa microfrontends into the micro container to ensure that the navigation is always the first element rendered

--- frontends/omnia-container/src/index.ts
+++ frontends/omnia-container/src/index.ts
@@ -37,11 +37,50 @@ export default function loadContainer(props: RequiredProps) {
     };
     registerApplication(app);
   });
+
+  const addHeadLink = (url: string, linkType: string, rel: string) => {
+    const link = document.createElement('link');
+    link.type = linkType;
+    link.rel = rel;
+    link.href = url;
+    document.head.appendChild(link);
+  };
+  // Inject fonts and icon styles into the doc head
+  addHeadLink('https://fonts.gstatic.com', '', 'preconnect');
+  addHeadLink(
+    'https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap',
+    '',
+    'stylesheet',
+  );
+  addHeadLink('https://use.typekit.net/vst7xer.css', '', 'stylesheet');
+  addHeadLink(
+    'https://fonts.googleapis.com/icon?family=Material+Icons',
+    '',
+    'stylesheet',
+  );
+
+  // Active Single Spa Layout and start
   layoutEngine.activate();
   start();
+
+  // Append all single spa apps to the micro container div
   window.addEventListener('single-spa:routing-event', () => {
     const target = document.getElementById('micro');
-    const spas = document.querySelectorAll("[id^='single-spa-application:']");
-    spas.forEach((el) => target?.appendChild(el));
+    const allSpas = document.querySelectorAll(
+      "[id^='single-spa-application:']",
+    );
+    const spaNav = document.querySelector(
+      '[id="single-spa-application:@websummit-micro/summit-engine-nav"]',
+    );
+    // Append spaNav to container
+    target?.appendChild(spaNav);
+    // Append other spas to container
+    allSpas.forEach((el) => {
+      if (
+        el.attributes[0].id !==
+        'single-spa-application:@websummit-micro/summit-engine-nav'
+      )
+        target?.appendChild(el);
+    });
   });
 }
