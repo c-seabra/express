591a8b0f6a9c3ec2b4d14fcd40768b94033c111c
p-parafiniuk <pawel.parafiniuk@netguru.com>
2021-04-19 12:17:30 +0200

    feature(comp): TKT-1227 add WiP comment to Toggle field and some cleanup

--- packages/components/src/molecules/ToggleField.tsx
+++ packages/components/src/molecules/ToggleField.tsx
@@ -1,6 +1,10 @@
+/**
+ * Initial work for ToggleField component (WiP)
+ */
+
 import { Field, FieldProps } from 'formik';
 import React, { HTMLProps } from 'react';
-import styled, { css } from 'styled-components';
+import styled from 'styled-components';
 
 const FieldContainer = styled.div`
   display: flex;
@@ -11,7 +15,6 @@ const ToggleContainer = styled.div`
   vertical-align: middle;
 `;
 
-// const ToggleInput = styled.input`
 const ToggleInput = styled.input`
   height: 0;
   width: 0;
@@ -63,34 +66,12 @@ const LabelFieldContainer = styled.label`
   }
 `;
 
-// const Label = styled.div<{ required?: boolean }>`
-//   color: #091a46;
-//   font-size: 1rem;
-//   margin-bottom: 4px;
-//
-//   ${(props) =>
-//     props.required &&
-//     css`
-//       &:after {
-//         color: #e15554;
-//         content: '*';
-//       }
-//     `}
-// `;
-
-// const Error = styled.div`
-//   color: #e15554;
-//   font-size: 12px;
-//   margin-top: 4px;
-// `;
 type ToggleFieldProps = HTMLProps<HTMLInputElement> & { name: string };
 
 const ToggleField = ({
   className,
-  // label,
   name,
   required,
-  // value,
   disabled = false,
 }: ToggleFieldProps) => {
   return (
