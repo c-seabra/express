d70da05a6cf140bf80f9ae7a4ecba10fa2a4d1b0
p-parafiniuk <pawel.parafiniuk@netguru.com>
2021-04-21 17:42:59 +0200

    feature(comp): TKT-1263 introduce package form

---
+++ frontends/ticket-management/src/components/organisms/PackageForm.tsx
@@ -0,0 +1,159 @@
+import { Button } from '@websummit/components/src/atoms/Button';
+import CheckboxField from '@websummit/components/src/molecules/CheckboxField';
+import { FieldWrapper } from '@websummit/components/src/molecules/FormikModal';
+import {
+  useErrorSnackbar,
+  useSuccessSnackbar,
+} from '@websummit/components/src/molecules/Snackbar';
+import TextAreaField from '@websummit/components/src/molecules/TextAreaField';
+import TextInputField from '@websummit/components/src/molecules/TextInputField';
+import FormikForm from '@websummit/components/src/templates/FormikForm';
+import { Spacing } from '@websummit/components/src/templates/Spacing';
+import { toShortDateTime } from '@websummit/components/src/utils/time';
+import { useCommerceUpdateSaleMutation } from '@websummit/graphql/src/@types/operations';
+import COMMERCE_SALES_LIST from '@websummit/graphql/src/operations/queries/SalesCyclesList';
+import React from 'react';
+import styled from 'styled-components';
+import * as Yup from 'yup';
+
+import STATIC_MESSAGES from '../../../../ticket-support/src/lib/constants/messages';
+import {useAppContext, useRequestContext} from '../app/AppContext';
+
+const StyledInputField = styled(TextInputField)`
+  width: 48%;
+`;
+
+const FlexEnd = styled.div`
+  display: flex;
+  justify-content: flex-end;
+`;
+
+const InlineWrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+
+const Fieldset = styled.fieldset`
+  border: none;
+  padding: 0;
+`;
+
+type Props = {
+  prefillData: any;
+};
+
+export type PackageFormData = {
+  active: boolean;
+  description: string;
+  endDate: any;
+  id: string;
+  name: string;
+  startDate: any;
+};
+
+const validationSchema = Yup.object().shape({
+  active: Yup.boolean(),
+  description: Yup.string().nullable(),
+  endDate: Yup.date().required(STATIC_MESSAGES.VALIDATION.REQUIRED),
+  name: Yup.string().required(STATIC_MESSAGES.VALIDATION.REQUIRED),
+  startDate: Yup.date().required(STATIC_MESSAGES.VALIDATION.REQUIRED),
+});
+
+const PackageForm = ({ prefillData }: Props) => {
+  const context = useRequestContext();
+  const snackbar = useSuccessSnackbar();
+  const errorSnackbar = useErrorSnackbar();
+  const [updateCycle] = useCommerceUpdateSaleMutation({
+    context,
+    onCompleted: () => {
+      snackbar('Sale cycle updated');
+    },
+    onError: (e) => errorSnackbar(e.message),
+    refetchQueries: [{ context, query: COMMERCE_SALES_LIST }],
+  });
+
+  const initialValues = () => {
+    return {
+      active: prefillData.active,
+      description: prefillData.description,
+      endDate: toShortDateTime(prefillData.endDate),
+      name: prefillData.name,
+      startDate: toShortDateTime(prefillData.startDate),
+    };
+  };
+
+  const pickMutation = (formData: PackageFormData) => {
+    const input = {
+      active: formData.active,
+      description: formData.description ? formData.description.trim() : null,
+      endDate: new Date(formData.endDate).toISOString(),
+      name: formData.name.trim(),
+      startDate: new Date(formData.startDate).toISOString(),
+    };
+
+    return updateCycle({
+      variables: { commerceSale: input, id: prefillData.id },
+    });
+  };
+
+  const setMutation = (formData: PackageFormData) => {
+    return pickMutation(formData);
+  };
+
+  return (
+    <FormikForm
+      initialValues={initialValues()}
+      submitCallback={setMutation}
+      validationSchema={validationSchema}
+    >
+      <Fieldset disabled={false}>
+        <Spacing top="2rem">
+          <FieldWrapper>
+            <Spacing bottom="8px">
+              <TextInputField required label="Sale cycle name" name="name" />
+            </Spacing>
+          </FieldWrapper>
+
+          <FieldWrapper>
+            <InlineWrapper>
+              <StyledInputField
+                required
+                label="Start date"
+                name="startDate"
+                type="datetime-local"
+              />
+
+              <StyledInputField
+                required
+                label="End date"
+                name="endDate"
+                type="datetime-local"
+              />
+            </InlineWrapper>
+          </FieldWrapper>
+
+          <FieldWrapper>
+            <Spacing bottom="8px">
+              <TextAreaField
+                fieldHeight="80px"
+                label="Sale cycle description"
+                name="description"
+              />
+            </Spacing>
+          </FieldWrapper>
+
+          <FieldWrapper>
+            <Spacing bottom="8px">
+              <CheckboxField label="Active" name="active" />
+            </Spacing>
+          </FieldWrapper>
+          <FlexEnd>
+            <Button type="submit">Edit</Button>
+          </FlexEnd>
+        </Spacing>
+      </Fieldset>
+    </FormikForm>
+  );
+};
+
+export default PackageForm;
