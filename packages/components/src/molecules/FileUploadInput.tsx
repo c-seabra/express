43c26da469a6586290ed5edd25772628b912a398
p-parafiniuk <pawel.parafiniuk@netguru.com>
2021-06-02 11:39:17 +0200

    feat: TKT-1065 introduce file FileUploadInput component

---
+++ packages/components/src/molecules/FileUploadInput.tsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import styled from 'styled-components';
+
+import { SecondaryButton } from '../atoms/Button';
+
+const FlexCol = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const FileUploadButton = styled(SecondaryButton)`
+  input[type='file'] {
+    display: none;
+  }
+`;
+
+const StyledFileInput = styled.input`
+  opacity: 0;
+  width: 0.1px;
+  height: 0.1px;
+  position: absolute;
+`;
+
+type FileUploadInputProps = {
+  elementId: any;
+  label: string;
+  onUpload: any;
+};
+
+const FileUploadInput = ({
+  label,
+  elementId,
+  onUpload,
+}: FileUploadInputProps) => {
+  return (
+    <FlexCol>
+      <FileUploadButton as="label">
+        <StyledFileInput
+          accept=".csv"
+          id={elementId}
+          type="file"
+          onChange={onUpload}
+        />
+
+        {label}
+      </FileUploadButton>
+    </FlexCol>
+  );
+};
+
+export default FileUploadInput;
