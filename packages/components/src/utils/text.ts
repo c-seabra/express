11ed2023429f691b578455d66a92edd1621abb2d
p-parafiniuk <pawel.parafiniuk@netguru.com>
2021-06-04 15:28:42 +0200

    feat: TKT-1465 Introduce text helper

--- packages/components/src/utils/text.ts
+++ packages/components/src/utils/text.ts
@@ -1,3 +1,18 @@
 export const upperWord = (word: string): string => {
   return word.charAt(0).toUpperCase() + word.slice(1).toLowerCase();
 };
+
+/**
+ * Shorten string with length greater than limit
+ * eg. abc...xyz
+ * @param elem
+ * @param lengthLimit
+ */
+export const shortenString = (elem: string, lengthLimit = 15): string => {
+  if (elem.length > lengthLimit) {
+    const halfLimit = Math.round(lengthLimit / 2);
+    return `${elem.substr(0, halfLimit)}...${elem.substr(-halfLimit)}`;
+  }
+
+  return elem;
+};
