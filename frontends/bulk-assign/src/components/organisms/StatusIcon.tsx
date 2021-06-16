31d6afc926f7bbe738041197cd6b4eae177c9956
p-parafiniuk <pawel.parafiniuk@netguru.com>
2021-06-16 11:35:07 +0200

    feat: TKT-1009 Move comps to orgarnisms

--- frontends/bulk-assign/src/components/organisms/StatusIcon.tsx
+++ frontends/bulk-assign/src/components/organisms/StatusIcon.tsx
@@ -3,8 +3,8 @@ import React from 'react';
 import Alert from '../../lib/svgs/Alert';
 import CheckMark from '../../lib/svgs/CheckMark';
 import { StatusType } from '../assigneeItem/AssigneeItem';
-import Tooltip from '../tooltip/Tooltip';
-import Loader from './Loader';
+import Tooltip from './Tooltip';
+import Loader from '@websummit/components/src/atoms/Loader';
 
 const StatusIcon: React.FC<{ status: StatusType }> = ({
   status: { type, message },
