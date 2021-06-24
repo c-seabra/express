30ba6b3b9f6c4fe90e7f2d982bd66ba333e75c67
Artem <artem.ankudovich@websummit.com>
2021-06-24 20:05:17 +0100

    Fix styling of secondary delete overlay

--- frontends/calendar-admin/src/components/editEvent/EditEvent.jsx
+++ frontends/calendar-admin/src/components/editEvent/EditEvent.jsx
@@ -1,3 +1,4 @@
+import ButtonComponent from '@websummit/components/src/atoms/Button';
 import { DestructiveButton } from '@websummit/components/src/atoms/Button';
 import React, { useContext, useEffect, useState } from 'react';
 
@@ -12,7 +13,6 @@ import {
   FormWrapper,
   InviteesListItem,
   Overlay,
-  OverlayButton,
   OverlayButtons,
   RemoveButton,
   StyledButton,
@@ -102,149 +102,158 @@ const EditEvent = ({
 
   return (
     <>
-    <DestructiveButton
-      style={{
-        marginTop: '1em',
-        marginLeft: '75%',
-      }}
-      onClick={(e) => {
-        e.preventDefault();
-        setDeletePopupActive(true);
-      }}
-    >
-      Delete Event
-    </DestructiveButton>
-    <Form onSubmit={(e) => handleSubmit(e)}>
-      <FormWrapper>
-        <FormLabel>Title: </FormLabel>
-        <FormInput
-          type="text"
-          value={editedEvent.title !== undefined ? editedEvent.title : title}
-          onChange={(e) => handleSetEditedEvent('title', e.target.value)}
-        />
-      </FormWrapper>
-      <FormWrapper>
-        <FormLabel>Starts At: </FormLabel>
-        <FormInput
-          type="datetime-local"
-          value={
-            editedEvent.starts_at !== undefined
-              ? editedEvent.starts_at
-              : startsAt
-          }
-          onChange={(e) => handleSetEditedEvent('starts_at', e.target.value)}
-        />
-      </FormWrapper>
-      <FormWrapper>
-        <FormLabel>Ends At: </FormLabel>
-        <FormInput
-          type="datetime-local"
-          value={
-            editedEvent.ends_at !== undefined ? editedEvent.ends_at : endsAt
-          }
-          onChange={(e) => handleSetEditedEvent('ends_at', e.target.value)}
-        />
-      </FormWrapper>
-      <FormWrapper>
-        <FormLabel htmlFor="location">Location: </FormLabel>
-        <FormInput
-          id="location"
-          list="locations"
-          type="text"
-          value={locationName}
-          onChange={(e) => handleLocationChange(e)}
-        />
-        <datalist id="locations">
-          {locations?.map((loc, i) => (
-            <option key={i} value={loc.name}>
-              {loc.name}
-            </option>
-          ))}
-        </datalist>
-      </FormWrapper>
-      <FormWrapper>
-        <FormLabel htmlFor="event_format_id">Format: </FormLabel>
-        <FormSelect
-          id="event_format_id"
-          type="text"
-          value={formatName}
-          onChange={(e) => handleFormatChange(e)}
-        >
-          <option defaultChecked>Please select a format</option>
-          {formats?.map((format, i) => (
-            <option key={i} value={format.label}>
-              {format.label}
-            </option>
-          ))}
-        </FormSelect>
-      </FormWrapper>
-      <FormWrapper>
-        <FormLabel>Description: </FormLabel>
-        <FormTextArea
-          value={
-            editedEvent.description !== undefined
-              ? editedEvent.description
-              : description
-          }
-          onChange={(e) => handleSetEditedEvent('description', e.target.value)}
-        />
-      </FormWrapper>
-      <FormWrapper>
-        <FormLabel>Add attendances: </FormLabel>
-        <AddAttendance selections={selections} setSelections={setSelections} />
-      </FormWrapper>
-      <div>
-        {rsvps.map(
-          (rsvp) =>
-            !deletions.includes(rsvp.invitation.id) && (
-              <InviteesListItem key={rsvp.invitation.id}>
-                <span style={{ width: '90%' }}>
-                  {`${String(rsvp.attendance.data.person.first_name)} ${String(
-                    rsvp.attendance.data.person.last_name,
-                  )}`}
-                </span>
-                <RemoveButton
-                  onClick={() => {
-                    setDeletions([...deletions, rsvp.invitation.id]);
-                  }}
-                />
-              </InviteesListItem>
-            ),
+      <DestructiveButton
+        style={{
+          marginTop: '1em',
+          marginLeft: '75%',
+        }}
+        onClick={(e) => {
+          e.preventDefault();
+          setDeletePopupActive(true);
+        }}
+      >
+        Delete Event
+      </DestructiveButton>
+      <Form onSubmit={(e) => handleSubmit(e)}>
+        <FormWrapper>
+          <FormLabel>Title: </FormLabel>
+          <FormInput
+            type="text"
+            value={editedEvent.title !== undefined ? editedEvent.title : title}
+            onChange={(e) => handleSetEditedEvent('title', e.target.value)}
+          />
+        </FormWrapper>
+        <FormWrapper>
+          <FormLabel>Starts At: </FormLabel>
+          <FormInput
+            type="datetime-local"
+            value={
+              editedEvent.starts_at !== undefined
+                ? editedEvent.starts_at
+                : startsAt
+            }
+            onChange={(e) => handleSetEditedEvent('starts_at', e.target.value)}
+          />
+        </FormWrapper>
+        <FormWrapper>
+          <FormLabel>Ends At: </FormLabel>
+          <FormInput
+            type="datetime-local"
+            value={
+              editedEvent.ends_at !== undefined ? editedEvent.ends_at : endsAt
+            }
+            onChange={(e) => handleSetEditedEvent('ends_at', e.target.value)}
+          />
+        </FormWrapper>
+        <FormWrapper>
+          <FormLabel htmlFor="location">Location: </FormLabel>
+          <FormInput
+            id="location"
+            list="locations"
+            type="text"
+            value={locationName}
+            onChange={(e) => handleLocationChange(e)}
+          />
+          <datalist id="locations">
+            {locations?.map((loc, i) => (
+              <option key={i} value={loc.name}>
+                {loc.name}
+              </option>
+            ))}
+          </datalist>
+        </FormWrapper>
+        <FormWrapper>
+          <FormLabel htmlFor="event_format_id">Format: </FormLabel>
+          <FormSelect
+            id="event_format_id"
+            type="text"
+            value={formatName}
+            onChange={(e) => handleFormatChange(e)}
+          >
+            <option defaultChecked>Please select a format</option>
+            {formats?.map((format, i) => (
+              <option key={i} value={format.label}>
+                {format.label}
+              </option>
+            ))}
+          </FormSelect>
+        </FormWrapper>
+        <FormWrapper>
+          <FormLabel>Description: </FormLabel>
+          <FormTextArea
+            value={
+              editedEvent.description !== undefined
+                ? editedEvent.description
+                : description
+            }
+            onChange={(e) =>
+              handleSetEditedEvent('description', e.target.value)
+            }
+          />
+        </FormWrapper>
+        <FormWrapper>
+          <FormLabel>Add attendances: </FormLabel>
+          <AddAttendance
+            selections={selections}
+            setSelections={setSelections}
+          />
+        </FormWrapper>
+        <div>
+          {rsvps.map(
+            (rsvp) =>
+              !deletions.includes(rsvp.invitation.id) && (
+                <InviteesListItem key={rsvp.invitation.id}>
+                  <span style={{ width: '90%' }}>
+                    {`${String(
+                      rsvp.attendance.data.person.first_name,
+                    )} ${String(rsvp.attendance.data.person.last_name)}`}
+                  </span>
+                  <RemoveButton
+                    onClick={() => {
+                      setDeletions([...deletions, rsvp.invitation.id]);
+                    }}
+                  />
+                </InviteesListItem>
+              ),
+          )}
+        </div>
+        <FormWrapper>
+          <StyledButton
+            style={{
+              backgroundColor: '#fff',
+              color: '#0067e9',
+              marginLeft: '65%',
+            }}
+            type="button"
+            onClick={() => setEditPopupActive(false)}
+          >
+            Cancel
+          </StyledButton>
+          <StyledButton type="submit">Save</StyledButton>
+        </FormWrapper>
+        {deletePopupActive && (
+          <Overlay>
+            <h3>Are you sure you want to delete this event?</h3>
+            <OverlayButtons>
+              <ButtonComponent
+                type="primary"
+                onClick={() => {
+                  onDeleteEvent(eventId);
+                  setDeletePopupActive(false);
+                }}
+              >
+                Yes
+              </ButtonComponent>
+              <ButtonComponent
+                type="error"
+                onClick={() => setDeletePopupActive(false)}
+                >
+                No
+              </ButtonComponent>
+            </OverlayButtons>
+          </Overlay>
         )}
-      </div>
-      <FormWrapper>
-        <StyledButton
-          style={{
-            backgroundColor: '#fff',
-            color: '#0067e9',
-            marginLeft: '65%',
-          }}
-          type="button"
-          onClick={() => setEditPopupActive(false)}
-        >
-          Cancel
-        </StyledButton>
-        <StyledButton type="submit">Save</StyledButton>
-      </FormWrapper>
-      {deletePopupActive && (
-        <Overlay>
-          <h3>Are you sure you want to delete this event?</h3>
-          <OverlayButtons>
-            <OverlayButton
-              onClick={() => {
-                onDeleteEvent(eventId);
-                setDeletePopupActive(false);
-              }}
-            >
-              Yes
-            </OverlayButton>
-            <OverlayButton onClick={() => setDeletePopupActive(false)}>
-              No
-            </OverlayButton>
-          </OverlayButtons>
-        </Overlay>
-      )}
-    </Form>
+      </Form>
     </>
   );
 };
