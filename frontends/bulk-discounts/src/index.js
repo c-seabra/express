3a5fda468bfb3a880cfddbc325c2720e8b83d867
Marvin Hofmann <644950+reggaemuffin@users.noreply.github.com>
2021-07-02 13:31:53 +0100

    fix merge

--- frontends/bulk-discounts/src/index.js
+++ frontends/bulk-discounts/src/index.js
@@ -1,5 +1,3 @@
-export { default as ReactDOM } from 'react-dom';
-export { default as React } from 'react';
 export { default as StaffTickets } from './components/App';
 export {
   bootstrap,
@@ -7,3 +5,5 @@ export {
   unmount,
   update,
 } from './websummit-micro-bulk-discounts';
+export { default as React } from 'react';
+export { default as ReactDOM } from 'react-dom';
